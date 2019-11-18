d30420ccb0d470c2ecfc42921b1276c53f3b8252
chandan sharma <sharmachandan351@gmail.com>
2019-11-18 22:45:23 +0545

    post destroy function added and post detroy template  added

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -102,6 +102,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post deleted');
     }
 }
--- resources/views/posts/details.blade.php
+++ resources/views/posts/details.blade.php
@@ -8,6 +8,10 @@
     <p>{!!$posts->body!!}</p>
 
     <a href="/posts/{{$posts->id}}/edit" class="btn btn-primary">Edit</a>
+    {!!Form::open(['action'=>['PostController@destroy',$posts->id],'method'=>'POST','class'=>'pull-right'])!!}
+    {{Form::hidden('_method','DELETE')}}
+    {{Form::submit('Delete',['class'=>'btn btn-danger'])}}
+    {!!Form::close()!!}
 </div>
 
 @endsection
